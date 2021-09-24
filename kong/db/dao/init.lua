afe8f0ba4016b4a6e2378ae6ec934310dfdf5830
王高原 <gy.wang.1983@gmail.com>
2021-09-24 10:55:02 +0800

    fix cache key of schema which workspaceable is not true

--- kong/db/dao/init.lua
+++ kong/db/dao/init.lua
@@ -1488,7 +1488,7 @@ function DAO:cache_key(key, arg2, arg3, arg4, arg5, ws_id)
     error("key must be a string or an entity table", 2)
   end
 
-  if key.ws_id then
+  if key.ws_id and self.schema.workspaceable then
     ws_id = key.ws_id
   end
 
